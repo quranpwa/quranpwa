e00f1d138759dc2eba8c1362eee76e92d137bc9f
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-05-02 14:18:37 +0600

    do not need prompt for a refresh

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import App from './App.tsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import './index.css'
-import { registerSW } from "virtual:pwa-register";
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-    onNeedRefresh() {
-        if (confirm("Update available. Reload?")) {
-            updateSW(true);
-        }
-    },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
