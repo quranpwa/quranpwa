0d342d5dd29c29042ff88deff3c4066c11f8cd9c
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-05-27 14:49:31 +0600

    added time in sura header

--- src/components/SuraHeader.tsx
+++ src/components/SuraHeader.tsx
@@ -8,8 +8,12 @@ function SuraHeader({ quranData, suraIdx }: SuraHeaderProps) {
 
     return <>
         <h2 className="sura-name-calligraphy">{quran_karim_114_font_chars[suraIdx]}</h2>
-        <h2 className="sura-name-en ltr">{suraIdx + 1}. Sura {sura.tname}</h2>
-        <div className="sura-info text-center text-secondary ltr">{sura.type} | Ayats: {sura.ayas}</div>
+        <h2 className="sura-name-en ltr">{suraIdx + 1}. {sura.tname}</h2>
+        <div className="text-nowrap text-center text-secondary ltr">
+            <span className="emoji-icon me-1">{sura.type == 'Meccan' ? '🕋' : '🕌'}</span>
+            {sura.ayas} Ayats
+            <span className="emoji-icon ms-3 me-1">⏱️</span>{quranData.getLengthInMinutes(sura)} minutes
+        </div>
 
         {suraIdx != 0 && suraIdx != 8 &&
             <p className="bismillah-arabic">{quranData.ayats[0].arabicText}</p>}
