41d2b18a2dbcfdc3c6e51efc54c768f61353f92f
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-05-27 16:41:07 +0600

    code refactoring

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -3,30 +3,22 @@
 function ThemeSwitch() {
     const [, forceUpdate] = useReducer(x => x + 1, 0);
 
-    const getStoredTheme = () => localStorage.getItem('theme') ?? 'auto'
-    const setStoredTheme = (theme: string) => localStorage.setItem('theme', theme)
-
-    const getPreferredTheme = () => {
-        const storedTheme = getStoredTheme()
-        if (storedTheme) {
-            return storedTheme
-        }
-
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    }
+    const getStoredTheme = () => localStorage.getItem('theme') ?? 'auto';
+    const setStoredTheme = (theme: string) => localStorage.setItem('theme', theme);
 
     const setTheme = (theme: string) => {
         if (theme === 'auto') {
-            let prefersColorScheme = (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-            document.documentElement.setAttribute('data-bs-theme', prefersColorScheme)
+            let prefersColorScheme = (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+            document.documentElement.setAttribute('data-bs-theme', prefersColorScheme);
             document.documentElement.setAttribute('data-theme', prefersColorScheme);
         } else {
-            document.documentElement.setAttribute('data-bs-theme', theme)
+            document.documentElement.setAttribute('data-bs-theme', theme);
             document.documentElement.setAttribute('data-theme', theme);
         }
     }
 
-    setTheme(getPreferredTheme())
+    const currentTheme = getStoredTheme();
+    setTheme(currentTheme);
 
     const handleSwitchTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
         let theme = event.target.value ?? 'auto';
@@ -34,8 +26,6 @@ function ThemeSwitch() {
         setTheme(theme);
         forceUpdate();
     };
-
-    const currentTheme = getStoredTheme();
 
     return <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
         <input type="radio" className="btn-check" name="btn-theme-selection" id="btn-theme-auto" autoComplete="off" value="auto" checked={currentTheme === 'auto'} onChange={handleSwitchTheme} />
