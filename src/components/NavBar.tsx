a8285fe9dceabf9f88fc3c73a917be9bea61e8c1
Deowan Faruque Ahamad <dfaruque@gmail.com>
2024-07-01 12:46:22 +0600

    added "Go To Selection" button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -40,6 +40,13 @@ function NavBar({ quranData, navData, onNavigate }: NavBarProps) {
         triggerOnNavigate();
     };
 
+    const handleGoToSelection = () => {
+        let selectedAyatElement = document.getElementById(location.hash.substring(1));
+        if (selectedAyatElement) {
+            selectedAyatElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     const showMenuNavButton = false;
 
     return <nav className='navbar fixed-top px-2'>
@@ -139,6 +146,14 @@ function NavBar({ quranData, navData, onNavigate }: NavBarProps) {
                 {quranData.pages.map(item => <option key={item.serial} value={item.serial}>{item.serial}</option>)}
             </select>
         }
+
+        <button className="btn theme-colored border nav-btn ms-2" type="button"
+            title='Go To Selection'
+            onClick={handleGoToSelection}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-crosshair" viewBox="0 0 16 16">
+                <path d="M8.5.5a.5.5 0 0 0-1 0v.518A7 7 0 0 0 1.018 7.5H.5a.5.5 0 0 0 0 1h.518A7 7 0 0 0 7.5 14.982v.518a.5.5 0 0 0 1 0v-.518A7 7 0 0 0 14.982 8.5h.518a.5.5 0 0 0 0-1h-.518A7 7 0 0 0 8.5 1.018zm-6.48 7A6 6 0 0 1 7.5 2.02v.48a.5.5 0 0 0 1 0v-.48a6 6 0 0 1 5.48 5.48h-.48a.5.5 0 0 0 0 1h.48a6 6 0 0 1-5.48 5.48v-.48a.5.5 0 0 0-1 0v.48A6 6 0 0 1 2.02 8.5h.48a.5.5 0 0 0 0-1zM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+            </svg>
+        </button>
 
         <button className="btn theme-colored border nav-btn ms-auto" type="button" style={{ alignSelf: 'flex-end' }}
             data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
